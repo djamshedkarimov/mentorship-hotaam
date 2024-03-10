c44b7ff297575492fd31e1396e338a4b80783761
Mamlakat Karimov <mamlakatkarimov@Mamlakats-MacBook-Air.local>
2024-03-09 16:08:17 -0800

    learned print and input

---
+++ day-1/printing.py
@@ -0,0 +1,8 @@
+# Write your code below this line 👇
+print("hi guys")
+print("john" + " " + "doe\n")
+
+name = input("What is your name?\n")
+name_len = len(name)
+print(f"Your name is {name}")
+print(f"There are {name_len} letters in your name.")
